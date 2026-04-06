d1bedf841ee30f9430ef359769ce621b25adb4cd
MathiasD-P <mathias.dufresne-piche@mail.mcgill.ca>
2026-04-05 20:46:23 -0400

    Changed prox operator for l2.

--- TwoNormProx.m
+++ TwoNormProx.m
@@ -1,6 +1,8 @@
 function [ out ] = TwoNormProx(x1, b, t)
 % Computes the proximal operator of the 2 norm  squared at x1
-[numRows, numCols] = size(x1);
+% [numRows, numCols] = size(x1);
 
-out = (1/((1/t)-2))*((1/t)*(x1+b)-2*b);
+out = (x1 + 2*t*b) / (2*t+1);
+
+% out = (1/((1/t)-2))*((1/t)*(x1+b)-2*b);
 end
