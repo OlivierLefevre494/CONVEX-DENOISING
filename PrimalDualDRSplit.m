2a00d4d3007b1cb3c96a2fcf974c5657c5590990
MathiasD-P <mathias.dufresne-piche@mail.mcgill.ca>
2026-04-15 00:15:15 -0400

    Typo fixed in PrimalDualDR

--- PrimalDualDRSplit.m
+++ PrimalDualDRSplit.m
@@ -5,9 +5,9 @@ else
 
     iterants.x = NormalizeImage(iterants.p); % This is the prox of f
     if strcmp(problem, "l2")==1 % scaled prox for l1 and isonorm
-        iterants.z = cat(3, params.tprimaldualdr .* TwoNormProx(iterants.q(:,:,1), blurredimage./params.tprimaldualdr, 1/params.tprimaldualdr), params.tprimaldualdr .* IsoProx(iterants.q(:,:,2)./(params.tprimaldualdr), iterants.q(:,:,3)./(params.tprimaldualdr), params.gammal2/(params.tprimaldualdr)));
+        iterants.z = cat(3, params.tprimaldualdr .* TwoNormProx(iterants.q(:,:,1) ./params.tprimaldualdr, blurredimage, 1/params.tprimaldualdr), params.tprimaldualdr .* IsoProx(iterants.q(:,:,2)./(params.tprimaldualdr), iterants.q(:,:,3)./(params.tprimaldualdr), params.gammal2/(params.tprimaldualdr)));
     elseif strcmp(problem, "l1")==1 % scaled prox for l2 and isonorm
-        iterants.z = cat(3, params.tprimaldualdr .* OneNormProx(iterants.q(:,:,1), blurredimage./params.tprimaldualdr, 1/params.tprimaldualdr), params.tprimaldualdr .* IsoProx(iterants.q(:,:,2)./(params.tprimaldualdr), iterants.q(:,:,3)./(params.tprimaldualdr), params.gammal1/(params.tprimaldualdr)));
+        iterants.z = cat(3, params.tprimaldualdr .* OneNormProx(iterants.q(:,:,1) ./params.tprimaldualdr, blurredimage, 1/params.tprimaldualdr), params.tprimaldualdr .* IsoProx(iterants.q(:,:,2)./(params.tprimaldualdr), iterants.q(:,:,3)./(params.tprimaldualdr), params.gammal1/(params.tprimaldualdr)));
     end
 
 %     if strcmp(problem, "l2")==1 % scaled prox for l1 and isonorm
