85546a366c04cde609ce04bddac11a3a26a4ae20
MathiasD-P <mathias.dufresne-piche@mail.mcgill.ca>
2026-04-21 16:43:35 -0400

    Primal Dual clean-up

--- PrimalDualDRUpdate.m
+++ PrimalDualDRUpdate.m
@@ -10,20 +10,14 @@ else
         iterants.z = cat(3, params.tprimaldualdr .* OneNormProx(iterants.q(:,:,1) ./params.tprimaldualdr, blurredimage, 1/params.tprimaldualdr), params.tprimaldualdr .* IsoProx(iterants.q(:,:,2)./(params.tprimaldualdr), iterants.q(:,:,3)./(params.tprimaldualdr), params.gammal1/(params.tprimaldualdr)));
     end
 
-%     if strcmp(problem, "l2")==1 % scaled prox for l1 and isonorm
-%         iterants.z = cat(3, params.tprimaldualdr .* TwoNormProx(iterants.q(:,:,1), blurredimage./params.tprimaldualdr, 1/params.tprimaldualdr), params.tprimaldualdr .* IsoProx(iterants.q(:,:,2)./(params.tprimaldualdr), iterants.q(:,:,3)./(params.tprimaldualdr), params.gammal2/(params.tprimaldualdr)));
-%     elseif strcmp(problem, "l1")==1 % scaled prox for l2 and isonorm
-%         iterants.z = cat(3, params.tprimaldualdr .* OneNormProx(iterants.q(:,:,1), blurredimage./params.tprimaldualdr, 1/params.tprimaldualdr), params.tprimaldualdr .* IsoProx(iterants.q(:,:,2)./(params.tprimaldualdr), iterants.q(:,:,3)./(params.tprimaldualdr), params.gammal1/(params.tprimaldualdr)));
-%     end
-
-    % prox for conjugate of l1 and isonorm (we use Moreau identity)
+    % prox for conjugate of l1/l2 and isonorm (we use Moreau identity)
     iterants.z = iterants.q - iterants.z;
     
+    % these are the DR split updates
     iterants.w = invertMatrix(2.0 .* iterants.x - iterants.p - params.tprimaldualdr .* ApplyATrans(2.0 .* iterants.z - iterants.q));
     iterants.v = params.tprimaldualdr .* ApplyA(iterants.w) + 2.0 .* iterants.z - iterants.q;
     iterants.p = iterants.p + params.rhoprimaldualdr .* (iterants.w - iterants.x);
     iterants.q = iterants.q + params.rhoprimaldualdr .* (iterants.v - iterants.z);
-    
     
     out = iterants;
 end
